ed72435b09276b7da8a44331c9da1eded2159fea
Pat Sabpisal <ssabpisa@me.com>
2016-08-29 00:13:17 +0700

    use real carousel

--- routes/views/index.js
+++ routes/views/index.js
@@ -8,9 +8,7 @@ exports = module.exports = function (req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
-	var gcontent = keystone.list('Gallery').model.find({
-		'name': 'HomeCarousel'
-	}).sort('sortOrder');
+	var gcontent = keystone.list('Carousel').model.find().sort('sortOrder');
 	view.query('gallery', gcontent);
 
 	// Render the view
